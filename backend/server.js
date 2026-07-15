598185c748c3369a8c0987886026bf64042eac03
BUdayKumarReddy <budayreddy8@gmail.com>
2026-07-15 07:33:20 +0000

    i changed everything related to pdf

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,28 @@
-require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const db = require('./db');
 
 const app = express();
 
-app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("Backend Running Successfully");
+    res.send("Indian Recipes Backend Running Successfully");
 });
 
-app.get('/users', (req, res) => {
-    db.query("SELECT * FROM users", (err, results) => {
+app.get('/recipes', (req, res) => {
+
+    db.query("SELECT * FROM recipes", (err, results) => {
+
         if (err) {
-            console.error(err);
-            return res.status(500).send("Database Query Failed");
+            return res.status(500).json({
+                error: err.message
+            });
         }
 
         res.json(results);
+
     });
+
 });
 
 app.listen(3000, () => {
